17d55596f83c0a0788e07184ce13891f9b01b1e9
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-10-07 10:45:12 +0600

    navbar update

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -10,78 +10,45 @@ const Navbar = () => {
                 <div className="text-white font-bold text-xl">GameOasis</div>
 
                 <ul className='text-white flex gap-5 font-semibold'>
-                    <li>Home</li>
-                    <li>All jewelry</li>
-                    <li>My Jewelry</li>
-                    <li>Add jewelry</li>
-                    <li>Blogs</li>
+                    <Link>
+                        <li>Home</li>
+                    </Link>
+                    <Link>
+                        <li>All jewelry</li>
+                    </Link>
+                    <Link>
+                        <li>My Jewelry</li>
+                    </Link>
+                    <Link>
+                        <li>Add jewelry</li>
+                    </Link>
+                    <Link>
+                        <li>Blogs</li>
+                    </Link>
                 </ul>
 
                 {/* Right part */}
                 <div className="flex space-x-4">
-                    <Link href="#">
                     
-                            <svg
-                                className=" h-4 w-4"
-                                fill="none"
-                                height="24"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                width="24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <rect height="9" rx="1" width="7" x="3" y="3" />
-                                <rect height="5" rx="1" width="7" x="14" y="3" />
-                                <rect height="9" rx="1" width="7" x="14" y="12" />
-                                <rect height="5" rx="1" width="7" x="3" y="16" />
-                            </svg>
-                            <span className="sr-only">Dashboard</span>
-                       
-                    </Link>
                     <Link href="#">
-                     
-                            <svg
-                                className=" h-4 w-4"
-                                fill="none"
-                                height="24"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                width="24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                                <circle cx="9" cy="7" r="4" />
-                                <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                            </svg>
-                            <span className="sr-only">Users</span>
-                      
-                    </Link>
-                    <Link href="#">
-                    
-                            <svg
-                                className=" h-4 w-4"
-                                fill="none"
-                                height="24"
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                viewBox="0 0 24 24"
-                                width="24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-                                <circle cx="12" cy="12" r="3" />
-                            </svg>
-                            <span className="sr-only">Settings</span>
-                        
+
+                        <svg
+                            className=" h-6 w-6"
+                            fill="#FFF"
+                            height="24"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+                            <circle cx="12" cy="12" r="3" />
+                        </svg>
+                        <span className="sr-only">Settings</span>
+
                     </Link>
                 </div>
             </div>
